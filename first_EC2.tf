f0821d239660152ada7a9bdcc124a03447cfd514
shivam1085 <44773461+shivam1085@users.noreply.github.com>
2023-01-28 01:20:11 +0530

    Update first_EC2.tf

--- first_EC2.tf
+++ first_EC2.tf
@@ -1,4 +1,16 @@
 provider "aws" {
-region = "us-west-2"
+region = "ap-northeast-1"
+access_key = "AKIAVI4SA3FOWCITGQGJ"
+secret_key = "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"
+
+
+}
+
+#
+
+resource "aws_instance" "newEC2" {
+
+    ami = "ami-06ee4e2261a4dc5c3"
+    instance_type = "t2.micro"
   
 }
